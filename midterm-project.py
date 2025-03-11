4283b3720c871cfd664c79bb5f9f1fb8e6c7ea37
Eunyoung Kim <ekim46@charlotte.edu>
2025-03-11 07:29:48 -0400

    fix: for deployment

--- midterm-project.py
+++ midterm-project.py
@@ -5,7 +5,6 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import plotly.express as px
 
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
