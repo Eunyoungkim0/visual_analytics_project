1ac4544073c2efe89861045942e5d7589744e77f
Andreas Lambropoulos <andlamb2002@gmail.com>
2025-04-13 03:01:40 -0400

    refactor: Split tabs into separate functions

--- midterm-project.py
+++ midterm-project.py
@@ -36,7 +36,7 @@ numeric_cols = [
 ]
 categorical_cols = ['BMI Category', 'Sleep Disorder']
 filter_cols = ['Gender', 'Age', 'Occupation']
-order_dict = {"BMI Category": ["Underweight", "Normal", "Overweight", "Obese"],
+order_dict = {"BMI Category": ["Normal", "Overweight", "Obese"],
                 "Sleep Disorder": ["None", "Sleep Apnea", "Insomnia"]}
 df_encoded = pd.get_dummies(df, columns=['Gender', 'Occupation', 'BMI Category', 'Sleep Disorder'])
 corr_matrix = df_encoded.corr()
@@ -53,22 +53,55 @@ def categorize_daily_steps(x, bins):
 def categorize_heart_rate(x, bins):
     return pd.cut([x], bins=bins, labels=[f"{b.left:.1f}-{b.right:.1f}" for b in bins[:-1]], include_lowest=True)[0]
 
-# tabs for navigation
-tab1, tab2, tab3, tab4 = st.tabs([
-    "Dataset analysis",
-    "View filtered data",
-    "Check correlation by variable",
-    "Linear regression analysis"
-])
+def filter_date(numeric_option, col):
 
-with tab1:
+    if col == 'Age':
+        num_bins = st.slider("Select number of bins:", min_value=2, max_value=6, value=4)
+        min_age = df['Age'].min()
+        max_age = df['Age'].max()
+
+        bins = np.linspace(min_age, max_age, num_bins + 1).astype(int)
+        labels = [f"{bins[i]}-{bins[i+1]-1}" for i in range(len(bins)-1)]    
+        df['Age Group'] = pd.cut(df['Age'], bins=bins, labels=labels, right=False)
+        col = 'Age Group'
+
+    df_group = df.groupby(f'{col}', observed=False)[numeric_option].mean()
+    df_melted = df_group.reset_index().melt(id_vars=[f'{col}'], var_name="Metric", value_name="Value")
+
+    st.write(f"#### Comparison by {col}")
+    chart = alt.Chart(df_melted).mark_bar().encode(
+        x=alt.X(f'{col}:N', title=f'{col}', axis=alt.Axis(labelAngle=-45)),
+        y=alt.Y("Value:Q", title="Average Value"),
+        color="Metric:N", 
+        xOffset="Metric:N"
+    ).properties(width=800, height=500)
+
+    st.altair_chart(chart)
+
+    for category in categorical_cols:
+        st.write(f"#### Distribution of {category} by {col}")
+        
+        df_count = df.groupby([f'{col}', category], observed=False).size().reset_index(name="Count")
+        sort_order = order_dict.get(category, None)
+
+        chart = alt.Chart(df_count).mark_bar().encode(
+            x=alt.X(f'{col}:N', title=f'{col}', axis=alt.Axis(labelAngle=-45), sort=sort_order),
+            y=alt.Y("Count:Q", title="Count"),
+            color=alt.Color(f"{category}:N", title=category, sort=sort_order),
+            xOffset=alt.XOffset(f"{category}:N", sort=sort_order)
+        ).properties(width=800, height=500)
+
+        st.altair_chart(chart)
+
+def dataset_analysis():
     st.write("## Dataset Analysis")
     if st.checkbox('Show dataframe'):
         st.write(df)
 
-    col1, col2 = st.columns(2) 
+    col1, col2, col3 = st.columns(3) 
 
     with col1:
+
         chart1 = alt.Chart(df).mark_bar().encode(
             x="Gender:N", 
             y="count():Q",
@@ -80,15 +113,8 @@ with tab1:
             x="Occupation:N", 
             y="count():Q",
             color=alt.Color("Occupation:N", legend=None)
-        ).properties(width=300, height=470, title='Occupation Distribution')
+        ).properties(width=300, height=320, title='Occupation Distribution')
         st.altair_chart(chart3)
-
-        chart4 = alt.Chart(df).mark_bar().encode(
-            x=alt.X("BMI Category:N", sort=order_dict["BMI Category"]), 
-            y="count():Q",
-            color=alt.Color("BMI Category:N", legend=None)
-        ).properties(width=300, height=320, title='BMI Category Distribution')
-        st.altair_chart(chart4)
 
         chart5 = alt.Chart(df).mark_bar().encode(
             x=alt.X("Stress Level:O", title="Stress Level", sort="ascending"),
@@ -96,13 +122,6 @@ with tab1:
             color=alt.Color("Stress Level:N", legend=None)
         ).properties(width=300, height=320, title="Stress Level Distribution")
         st.altair_chart(chart5)
-
-        chart6 = alt.Chart(df).mark_bar().encode(
-            x=alt.X("Quality of Sleep:O", title="Quality of Sleep", sort="ascending"),
-            y="count():Q",
-            color=alt.Color("Quality of Sleep:N", legend=None)
-        ).properties(width=300, height=320, title="Quality of Sleep Distribution")
-        st.altair_chart(chart6)
 
         chart7 = alt.Chart(df).mark_bar().encode(
             x=alt.X("Sleep Disorder:N", sort=order_dict["Sleep Disorder"]), 
@@ -112,12 +131,11 @@ with tab1:
         st.altair_chart(chart7)
         
     with col2:
-        
+
         age_bins = [25, 30, 40, 50, 60, 70]
         age_labels = ["20's", "30's", "40's", "50's", "60's'"]
         df["Age Group"] = pd.cut(df["Age"], bins=age_bins, labels=age_labels, right=False)
         df["Age Group"] = pd.Categorical(df["Age Group"], categories=age_labels, ordered=True)
-
         chart2 = alt.Chart(df).mark_bar().encode(
             x=alt.X("Age Group:N", sort=age_labels),
             y="count():Q",
@@ -125,9 +143,36 @@ with tab1:
         ).properties(width=300, height=320, title='Age Group Distribution')
         st.altair_chart(chart2)
 
+        chart4 = alt.Chart(df).mark_bar().encode(
+            x=alt.X("BMI Category:N", sort=order_dict["BMI Category"]), 
+            y="count():Q",
+            color=alt.Color("BMI Category:N", legend=None)
+        ).properties(width=300, height=320, title='BMI Category Distribution')
+        st.altair_chart(chart4)
+
+        chart6 = alt.Chart(df).mark_bar().encode(
+            x=alt.X("Quality of Sleep:O", title="Quality of Sleep", sort="ascending"),
+            y="count():Q",
+            color=alt.Color("Quality of Sleep:N", legend=None)
+        ).properties(width=300, height=320, title="Quality of Sleep Distribution")
+        st.altair_chart(chart6)
+
+        sleep_bins = [5, 6, 7, 8, 9]
+        sleep_labels = ["5-6", "6-7", "7-8", "8-9"]
+        df["Sleep Duration Group"] = pd.cut(df["Sleep Duration"], bins=sleep_bins, labels=sleep_labels, right=False)
+        df_group = df.groupby("Sleep Duration Group", observed=False).size().reset_index(name="Count")
+        chart8 = alt.Chart(df_group).mark_bar().encode(
+            x=alt.X("Sleep Duration Group:N", title="Hours of Sleep", sort=sleep_labels),
+            y=alt.Y("Count:Q", title="Count"),
+            color=alt.Color("Sleep Duration Group:N", legend=None)
+        ).properties(width=300, height=320, title="Sleep Duration Distribution")
+        st.altair_chart(chart8)
+
+    with col3:
+
         st.write("Select number of bins for categories below")
         num_bins = st.slider("Number of bins", 2, 6, 4)
-        bin_cols = ["Sleep Duration", "Physical Activity Level", "Daily Steps", "Heart Rate"]
+        bin_cols = ["Physical Activity Level", "Daily Steps", "Heart Rate"]
 
         for col in bin_cols:
             group_col = str(col) + " Group"
@@ -141,7 +186,7 @@ with tab1:
             labels = [f"{bins[i]}-{bins[i+1]-1}" for i in range(len(bins)-1)]    
 
             df[group_col] = pd.cut(df[col], bins=bins, labels=labels, right=False, duplicates='drop')
-            df_group = df.groupby(group_col).size().reset_index(name="Count")
+            df_group = df.groupby(group_col, observed=False).size().reset_index(name="Count")
             df_melted = df_group.melt(id_vars=[group_col], var_name="Metric", value_name="Value")
 
             chart = alt.Chart(df_melted).mark_bar().encode(
@@ -152,109 +197,19 @@ with tab1:
 
             st.altair_chart(chart)
 
-with tab2:
+def view_filtered_data():
     st.write("## View Filtered Data")
 
-    numeric_option = st.multiselect('Which factors would you like to view? (Numeric Values)', numeric_cols, numeric_cols[0])
-    filter_by_option = st.selectbox('How would you like to group the data?', filter_cols)
+    col1, col2 = st.columns(2)
+    with col1:
+        numeric_option = st.multiselect('Which factors would you like to view? (Numeric Values)', numeric_cols, numeric_cols[0])
+    with col2:
+        filter_by_option = st.selectbox('How would you like to group the data?', filter_cols)
     st.write("###")
 
-    if filter_by_option == 'Gender':
-        
-        df_gender = df.groupby('Gender')[numeric_option].mean()
-        
-        df_melted = df_gender.reset_index().melt(id_vars=["Gender"], var_name="Metric", value_name="Value")
-
-        st.write(f"#### Comparison by Gender")
-        chart = alt.Chart(df_melted).mark_bar().encode(
-            x=alt.X("Gender:N", title="Gender", axis=alt.Axis(labelAngle=-45)),
-            y=alt.Y("Value:Q", title="Average Value"),
-            color="Metric:N", 
-            xOffset="Metric:N"
-        ).properties(width=800, height=500)
-
-        st.altair_chart(chart)
-
-        for category in categorical_cols:
-            st.write(f"#### Distribution of {category} by Gender")
-            
-            df_count = df.groupby(["Gender", category]).size().reset_index(name="Count")
-
-            chart = alt.Chart(df_count).mark_bar().encode(
-                x=alt.X("Gender:N", title="Gender", axis=alt.Axis(labelAngle=-45)),
-                y=alt.Y("Count:Q", title="Count"),
-                color=alt.Color(f"{category}:N", title=category),
-                xOffset=alt.XOffset(f"{category}:N")
-            ).properties(width=800, height=500)
-
-            st.altair_chart(chart)
-            
-    elif filter_by_option == 'Age':
-        
-        num_bins = st.slider("Select number of bins:", min_value=2, max_value=6, value=4)
-        min_age = df['Age'].min()
-        max_age = df['Age'].max()
-
-        bins = np.linspace(min_age, max_age, num_bins + 1).astype(int)
-        labels = [f"{bins[i]}-{bins[i+1]-1}" for i in range(len(bins)-1)]    
-        df['Age Group'] = pd.cut(df['Age'], bins=bins, labels=labels, right=False)
-
-        df_age = df.groupby('Age Group')[numeric_option].mean()
-        df_melted = df_age.reset_index().melt(id_vars=["Age Group"], var_name="Metric", value_name="Value")
-
-        chart = alt.Chart(df_melted).mark_bar().encode(
-            x=alt.X("Age Group:N", title="Age Group", axis=alt.Axis(labelAngle=-45)),
-            y=alt.Y("Value:Q", title="Average Value"),
-            color="Metric:N", 
-            xOffset="Metric:N"
-        ).properties(width=800, height=500, title="Comparison by Age Group")
-
-        st.altair_chart(chart)
-
-        for category in categorical_cols:
-            st.write(f"#### Distribution of {category} by Age Group")
-            
-            df_count = df.groupby(["Age Group", category]).size().reset_index(name="Count")
-
-            chart = alt.Chart(df_count).mark_bar().encode(
-                x=alt.X("Age Group:N", title="Age Group", axis=alt.Axis(labelAngle=-45)),
-                y=alt.Y("Count:Q", title="Count"),
-                color=alt.Color(f"{category}:N", title=category),
-                xOffset=alt.XOffset(f"{category}:N")
-            ).properties(width=800, height=500)
-
-            st.altair_chart(chart)
-
-    elif filter_by_option == 'Occupation':
-        
-        df_occupation = df.groupby('Occupation')[numeric_option].mean()
-        
-        df_melted = df_occupation.reset_index().melt(id_vars=["Occupation"], var_name="Metric", value_name="Value")
-
-        chart = alt.Chart(df_melted).mark_bar().encode(
-            x=alt.X("Occupation:N", title="Occupation"),
-            y=alt.Y("Value:Q", title="Average Value"),
-            color="Metric:N", 
-            xOffset="Metric:N"
-        ).properties(width=800, height=500, title="Comparison by Occupation")
-
-        st.altair_chart(chart)
-
-        for category in categorical_cols:
-            st.write(f"#### Distribution of {category} by Occupation")
-            
-            df_count = df.groupby(["Occupation", category]).size().reset_index(name="Count")
-
-            chart = alt.Chart(df_count).mark_bar().encode(
-                x=alt.X("Occupation:N", title="Occupation"),
-                y=alt.Y("Count:Q", title="Count"),
-                color=alt.Color(f"{category}:N", title=category),
-                xOffset=alt.XOffset(f"{category}:N")
-            ).properties(width=800, height=500)
-
-            st.altair_chart(chart)
+    filter_date(numeric_option, filter_by_option)
     
-with tab3:
+def check_correlation_by_variable():
     st.write("## Check Correlation by Variable")
 
     st.write("### Correlation Matrix Heatmap")
@@ -264,7 +219,7 @@ with tab3:
     st.pyplot(fig)
 
     st.write("### Select correlation threshold")
-    threshold = st.slider("", 0.0, 1.0, 0.6, 0.1)
+    threshold = st.slider("Correlation Threshold", 0.0, 1.0, 0.6, 0.1)
     filtered_corr = corr_matrix[(corr_matrix >= threshold) | (corr_matrix <= -threshold)]
 
     st.write(f"### Filtered Correlation Matrix (|corr| ≥ {threshold})")
@@ -274,8 +229,7 @@ with tab3:
             linewidths=0.5, linecolor='black', mask=filtered_corr.isna())
     st.pyplot(fig)
 
-with tab4:
-
+def linear_regression_analysis():
     st.write("## Linear Regression Analysis")
     
     correlation_matrix = df[numeric_cols].corr().abs()
@@ -342,7 +296,22 @@ with tab4:
         ax.set_title("Residuals Distribution")
         st.pyplot(fig)
 
-# else:
+# tabs for navigation
+tab1, tab2, tab3, tab4 = st.tabs([
+    "Dataset analysis",
+    "View filtered data",
+    "Check correlation by variable",
+    "Linear regression analysis"
+])
 
-#     st.markdown("### Select a category from the left side to view the chart. :)")
-#     st.stop()
+with tab1:
+    dataset_analysis()
+
+with tab2:
+    view_filtered_data()
+
+with tab3:
+    check_correlation_by_variable()
+
+with tab4:
+    linear_regression_analysis()
